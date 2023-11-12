a682d0b16e4a8cca785cda6cbe6760c2d7cb0854
1x55 <k4i@pm.me>
2023-11-12 10:46:58 +0900

    amended image size to better suit container

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,7 +1,11 @@
 export default function MenuItem() {
     return (
-        <div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white hover:shadow-2xl hover:shadow-black transition-all">   
-        <img src = "/ramen.png" alt="ramen"/>
+        <div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white hover:shadow-md hover:shadow-black/25 transition-all">   
+
+        <div className="text-center">
+            <img src = "/ramen.png" className="max-h-auto max-h-10" alt="ramen"/>
+        </div>
+        
         <h4 className="font-semibold text-xl my-3">Tonkotsu Ramen</h4>
         <p className="text-gray-500 text-sm">The soul of Tonkotsu Ramen. Pork bones are boiled for hours until they break down, creating a creamy, white broth. The long cooking time extracts collagen, marrow, and fat, giving the broth its signature richness and depth.
         </p>
