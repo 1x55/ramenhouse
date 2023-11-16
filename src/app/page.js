384b2868b7bf66a90b3e5bcbaacdc4a4cc353565
1x55 <k4i@pm.me>
2023-11-16 13:26:22 +0900

    added flex and padding ot nav and logo, removed border

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,11 @@ import Link from "next/link"
 export default function Home() {
   return (
     <>
-      <header>
+      <header className="flex items-center justify-between">
         <Link className="text-primary font-semibold text-2xl" href="">
           Ramen
         </Link>
-        <nav>
+        <nav className="flex gap-4">
           <Link href={''}>Home</Link>
           <Link href={''}>Menu</Link>
           <Link href={''}>About</Link>
