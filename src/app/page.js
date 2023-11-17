6e5db1ae8b18c3a97725858dc4536effef176ac9
1x55 <k4i@pm.me>
2023-11-17 13:32:46 +0900

    styled login button

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,12 @@ export default function Home() {
         <Link className="text-primary font-semibold text-2xl" href="">
           Ramen
         </Link>
-        <nav className="flex gap-4">
+        <nav className="flex items-center gap-4 text-gray-500 font-semi-bold">
           <Link href={''}>Home</Link>
           <Link href={''}>Menu</Link>
           <Link href={''}>About</Link>
           <Link href={''}>Contact</Link>
-          <Link href={''}>Login</Link>
+          <Link href={''} className="bg-primary rounded-full text-white px-6 py-2">Login</Link>
         </nav>
       </header>
     </>
