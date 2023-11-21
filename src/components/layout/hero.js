6b8175c82ee63e2c61f50414e20d3fe91f560ec8
1x55 <k4i@pm.me>
2023-11-21 15:49:17 +0900

    testing withimages

--- src/components/layout/hero.js
+++ src/components/layout/hero.js
@@ -1,8 +1,11 @@
+import Image from "next/image";
 export default function Hero() {
     return (
     <section>
         <h1>Life if Better with a Bowl of Ramen</h1>
-        <p>Ramen is the missing piece that makes every day complete, a simple yet delicious joy in liofe</p>
+        <p>Ramen is the missing piece that makes every day complete, a simple yet delicious joy in liofe
+        </p>
+        <Image src={'/ramen.png'} layout={'fill'} objectFit={'contain'} alt ={'ramen'} />
     </section>
  );
 }
