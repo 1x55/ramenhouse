792493d9f1751834fd071e444b8f4bf29407fb8e
1x55 <k4i@pm.me>
2023-11-23 16:01:07 +0900

    added 2 col grid

--- src/components/layout/hero.js
+++ src/components/layout/hero.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 export default function Hero() {
     return (
-    <section>
-        <h1>Life if Better with a Bowl of Ramen</h1>
-        <p>Ramen is the missing piece that makes every day complete, a simple yet delicious joy in liofe
-        </p>
-        <div className="w-16 h-16 relative">
+    <section className="grid grid-cols-2">
+        <div>
+            <h1 className="text-4xl font-semibold">
+                Life if Better with a Bowl of Ramen
+            </h1>
+            <p className="mt-4">Ramen is the missing piece that makes every day complete, a simple yet delicious joy in liofe
+            </p>
+        </div>
+        <div className="relative">
             <Image src={'/ramen.png'} layout={'fill'} objectFit={'contain'} alt ={'ramen'} />
         </div>
     </section>
