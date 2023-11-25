80cb5c2ffb723ad462594c693b5648082a7ef8c0
1x55 <k4i@pm.me>
2023-11-25 16:28:26 +0900

    styled and aligned both buttons in hero section

--- src/components/layout/hero.js
+++ src/components/layout/hero.js
@@ -8,16 +8,17 @@ export default function Hero() {
             <h1 className="text-4xl font-semibold">
                 Life if Better with a Bowl of Ramen
             </h1>
-            <p className="my-4 text-gray-500">Ramen is the missing piece that makes every day complete, a simple yet delicious joy in liofe
+            <p className="my-4 text-gray-500">Ramen is the missing piece that makes every day complete, a simple yet delicious joy in life
             </p>
             {/* add button */}
-            <div className="flex gap-4">
-                <button className="bg-primary text-white px-8 py-2 rounded-full">
+            <div className="flex gap-4 text-sm">
+                <button className="bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
                     Order now
                     <Right />
                 </button>
-                <button>
+                <button className="flex gap-2 py-2 text-gray-500 font-semi-bold">
                     Learn more
+                    <Right />
                 </button>
             </div>
 
