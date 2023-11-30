4d3940e8f2c6063ad6b650c60a89b042e5c8223c
1x55 <k4i@pm.me>
2023-11-30 11:26:00 +0900

    added contact us

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,27 @@ export default function Home() {
          subHeader={'Our story'}
          mainHeader={'About us'}
         />
+
+        <div className="max-w-md mx-auto mt-4 text-gray-500 flex flex-col gap-4">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos fugiat suscipit tempore delectus explicabo sapiente aspernatur voluptatem, autem dignissimos velit molestiae, incidunt natus dolorum inventore alias quas aperiam atque expedita?</p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam deserunt iure nostrum, voluptatum odio dicta delectus assumenda, molestias incidunt optio!</p>
+
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, debitis. </p>
+        </div>
+      </section>
+
+      {/* add contact section */}
+      <section className="text-center my-8">
+        <SectionHeaders 
+          subHeader={'Don\'t hesitate'}
+          mainHeader={'Contact us'} 
+        />
+        <div className="mt-8">
+          <a className="text-4xl" href="tel:+81123123123">
+          +81 123 123 123 
+          </a>
+        </div>
       </section>
     </>
     )
